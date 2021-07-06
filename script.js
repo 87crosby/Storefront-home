871b15ca86726a2aee5e1525729ccf57c53a57db
87crosby <71791452+87crosby@users.noreply.github.com>
2021-07-06 16:20:12 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function changeImg(){
+    var pic = document.querySelector("#pic")
+    pic.src = "assets/succulents-2.jpg"
+}
+
+function hide(){
+    var hide = document.querySelector("#cookie")
+    hide.remove()
+}
+
+function cartAlert(){
+    alert("Your cart is empty!")
+}
